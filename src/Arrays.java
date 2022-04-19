7f769a7e40366998e6362cf65131d4ee436a60c2
justkelvin <muia.muinde@s.karu.ac.ke>
2022-04-19 16:25:02 +0300

    arrays lists

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Arrays {
     public static void main(String[] args) {
         // Fixed size array of 3
@@ -9,8 +11,21 @@ public class Arrays {
         System.out.println(myStringArray[2]);
 
         int[] myNumArray = {1,2,3,4,5,6,7};
-        
+
         System.out.println(myNumArray[4]);
+
+        // Array lists
+        ArrayList<String> myMusicians = new ArrayList<>();
+        myMusicians.add("Sia");
+        myMusicians.add("Kina");
+        myMusicians.add("Meg");
+        myMusicians.add(1, "Avicii");
+        System.out.println(myMusicians.get(0));
+        System.out.println(myMusicians.get(1));
+        System.out.println(myMusicians.get(2));
+        System.out.println(myMusicians.get(3));
+        
+        
     }
     
 }
