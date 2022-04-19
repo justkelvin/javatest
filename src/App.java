c752bcb548e6ab826a50955792a542614cd73d3c
justkelvin <muia.muinde@s.karu.ac.ke>
2022-04-19 15:33:53 +0300

    more about datatypes

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Variables
-        int age = 11;
-        double x = 3.2;
-        float r = 8.12f;
+        double pi = 3.14;
+        int radius = 5;
+        System.out.println(2 * radius * pi);
     }
 }
