f47e186b051781bd70651fdd95739409c78d67a3
justkelvin <muia.muinde@s.karu.ac.ke>
2022-04-19 15:36:42 +0300

    learn strings

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Variables
-        double pi = 3.14;
-        int radius = 5;
-        System.out.println(2 * radius * pi);
+        // double pi = 3.14;
+        // int radius = 5;
+        // // System.out.println(2 * radius * pi);
+
+        // Strings
+        String myName = "Kelvin";
+        String surName = "Muinde";
+        String fullName = myName + " " + surName;
+        System.out.println(myName);
+        System.out.println(surName);
+        System.out.println(fullName);
     }
 }
