19bca100b2b65547577f5da2571d8b1de8c3a3bf
justkelvin <muia.muinde@s.karu.ac.ke>
2022-04-19 15:28:34 +0300

    learn about datatypes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Variables
+        int age = 11;
+        double x = 3.2;
+        float r = 8.12f;
     }
 }
