921f69c65dd333cd3e9f5155516e3f150bd3f478
justkelvin <muia.muinde@s.karu.ac.ke>
2022-04-19 16:13:46 +0300

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Fixed size array of 3
+        String[] myStringArray = new String[3];
+        myStringArray[0] = "Useraname";
+        myStringArray[1] = "Password";
+        myStringArray[2] = "Email";
+
+        System.out.println(myStringArray[2]);
+
+        int[] myNumArray = {1,2,3,4,5,6,7};
+        
+        System.out.println(myNumArray[4]);
+    }
+    
+}
